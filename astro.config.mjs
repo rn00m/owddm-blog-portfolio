f9a1a211c35a6a1e063f1b05a02273b504e91a85
rn00m <romano@moesa.net>
2024-02-23 10:22:17 +0900

    Before re:factor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: process.env.NODE_ENV === "development" ? "http://localhost:4321" : "https://owddm.masscom.media",
+  integrations: [tailwind(), sitemap(), icon()]
 });
