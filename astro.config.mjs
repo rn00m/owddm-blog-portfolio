cfc9f7c725fa88b6e25e87f318b2cda57a0e7cb0
rn00m <romano@moesa.net>
2024-03-19 12:54:25 +0900

    Notice updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   site:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4321"
-      : "https://owddm.masscom.media",
+      : "https://owddm-workshop.netlify.app/",
   integrations: [tailwind(), sitemap(), icon()],
 });
